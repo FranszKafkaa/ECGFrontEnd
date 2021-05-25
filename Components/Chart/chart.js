6a5362611ef1b5a9354953201261c0d906de4d54
Marcelo da Silva Janke <marcelosjanke@icloud.com>
2021-05-25 19:44:55 -0300

    oi

--- Components/Chart/chart.js
+++ Components/Chart/chart.js
@@ -15,9 +15,6 @@ export class ChartDOM extends Component {
 		super(props);
 		console.log(this.props);
 
-        
-
-        
 		this.state = {
 			Component: null,
 			Another: null,
@@ -36,117 +33,117 @@ export class ChartDOM extends Component {
 	}
 
 	RenderBPM(bolo = 0, rpeakCount = 1, limite = 500, arr = []) {
-
-        const {route} = this.props;
-        let params = route.params
-        console.log(params)
-		axios.get("https://ecgremote.herokuapp.com/render?id="+params["id"]).then((result) => {
-			for (
-				var i = 0;
-				i < result.data.res.filtered.length;
-				i += result.data.res.rate / 5
-			) {
-				this.state.data.push(i);
-			}
-
-			var timer = setInterval((_) => {
-				if (!this.state.paused) {
-					if (bolo >= limite) {
-						arr = arr.slice(40, arr.length - 1);
-						limite += 40;
-					}
-
-					if (bolo >= result.data.res.filtered.length) {
-						clearInterval(timer);
-					}
-
-					arr.push(result.data.res.filtered[bolo++]);
-
-					if (bolo >= result.data.res.rpeaks[rpeakCount]) {
-						let bpm = (
-							<BPM>
-								<TextView>
-									BPM <br /> {parseInt(result.data.res.bpm[rpeakCount - 1])}
-								</TextView>
-							</BPM>
-						);
-						rpeakCount++;
-
-						this.setState({ bpm });
-					}
-
-					let el = (
-						<View
-							style={{
-								width: 600,
-							}}
-						>
-							<LineChart
-								style={{ height: 500, width: 600 }}
-								data={arr}
-								svg={{ stroke: "rgb(0, 255, 0)" }}
-								contentInset={{ top: 20, bottom: 20 }}
-								animate={false}
-							></LineChart>
-						</View>
-					);
-
-					this.setState({ Another: el });
+		const { route } = this.props;
+		let params = route.params;
+		console.log(params);
+		axios
+			.get("https://ecgremote.herokuapp.com/render?id=" + params["id"])
+			.then((result) => {
+				for (
+					var i = 0;
+					i < result.data.res.filtered.length;
+					i += result.data.res.rate / 5
+				) {
+					this.state.data.push(i);
 				}
+
+				var timer = setInterval((_) => {
+					if (!this.state.paused) {
+						if (bolo >= limite) {
+							arr = arr.slice(40, arr.length - 1);
+							limite += 40;
+						}
+
+						if (bolo >= result.data.res.filtered.length) {
+							clearInterval(timer);
+						}
+
+						arr.push(result.data.res.filtered[bolo++]);
+
+						if (bolo >= result.data.res.rpeaks[rpeakCount]) {
+							let bpm = (
+								<BPM>
+									<TextView>
+										BPM <br /> {parseInt(result.data.res.bpm[rpeakCount - 1])}
+									</TextView>
+								</BPM>
+							);
+							rpeakCount++;
+
+							this.setState({ bpm });
+						}
+
+						let el = (
+							<View
+								style={{
+									width: 600,
+								}}
+							>
+								<LineChart
+									style={{ height: 500, width: 600 }}
+									data={arr}
+									svg={{ stroke: "rgb(0, 255, 0)" }}
+									contentInset={{ top: 20, bottom: 20 }}
+									animate={false}
+								></LineChart>
+							</View>
+						);
+
+						this.setState({ Another: el });
+					}
+				});
+				const CustomGrid = ({ x, y, _, ticks }) => (
+					<G>
+						{
+							// Horizontal grid
+							ticks.map((tick) => (
+								<Line
+									key={tick}
+									x1={"0%"}
+									x2={"100%"}
+									y1={y(tick)}
+									y2={y(tick)}
+									stroke={"rgba(255,0,0)"}
+								/>
+							))
+						}
+
+						{
+							// Vertical grid
+							this.state.data.map((dado) => (
+								<Line
+									key={dado}
+									y1={"0%"}
+									y2={"100%"}
+									x1={x(dado)}
+									x2={x(dado)}
+									stroke={"rgba(0,250,0)"}
+								/>
+							))
+						}
+					</G>
+				);
+
+				let ele = (
+					<>
+						<LineChart
+							style={{ height: 210, width: Dimensions.get("window").width }}
+							data={result.data.res.filtered}
+							svg={{ stroke: "rgb(0, 0, 0)" }}
+							contentInset={{ top: 20, bottom: 20 }}
+						>
+							<CustomGrid belowChart={true} />
+						</LineChart>
+					</>
+				);
+
+				this.setState({ Component: ele });
 			});
-			const CustomGrid = ({ x, y, _, ticks }) => (
-				<G>
-					{
-						// Horizontal grid
-						ticks.map((tick) => (
-							<Line
-								key={tick}
-								x1={"0%"}
-								x2={"100%"}
-								y1={y(tick)}
-								y2={y(tick)}
-								stroke={"rgba(255,0,0)"}
-							/>
-						))
-					}
-
-					{
-						// Vertical grid
-						this.state.data.map((dado) => (
-							<Line
-								key={dado}
-								y1={"0%"}
-								y2={"100%"}
-								x1={x(dado)}
-								x2={x(dado)}
-								stroke={"rgba(0,250,0)"}
-							/>
-						))
-					}
-				</G>
-			);
-
-			let ele = (
-				<>
-					<LineChart
-						style={{ height: 210, width: Dimensions.get("window").width }}
-						data={result.data.res.filtered}
-						svg={{ stroke: "rgb(0, 0, 0)" }}
-						contentInset={{ top: 20, bottom: 20 }}
-					>
-						<CustomGrid belowChart={true} />
-					</LineChart>
-				</>
-			);
-
-			this.setState({ Component: ele });
-		});
 	}
 
 	render() {
 		return (
 			<ScrollView>
-
 				<Frag onClick={this.tooglePauseAnimation}>
 					{this.state.Another}
 
--- Components/list/list.js
+++ Components/list/list.js
@@ -7,49 +7,55 @@ export default class List extends Component {
 	constructor(props) {
 		super(props);
 
-        this.state = {
-            data: []
-        }
+		this.state = {
+			data: [],
+		};
 
-        this.getData()
+		this.getData();
 	}
 
-    getData(){
-        console.log("passo")
-        axios.get("https://ecgremote.herokuapp.com/list_all").then(result => {
-            const {data} = result
+	getData() {
+		console.log("passo");
+		axios.get("https://ecgremote.herokuapp.com/list_all").then((result) => {
+			const { data } = result;
 
-            var arr = []
+			var arr = [];
 
-            const {navigation} = this.props;
+			const { navigation } = this.props;
 
+			data.forEach((element) => {
+				console.log(element);
 
-            data.forEach(element => {
-                console.log(element)
-
-                arr.push((
-                    <>
-                    <DataTable.Row>
-                        <DataTable.Cell onPress={() => navigation.navigate("chart", {
-                            id: element["_id"]
-                        })}>{element["_id"]}</DataTable.Cell>
-                        <DataTable.Cell onPress={() => navigation.navigate("chart", {
-                            id: element["_id"]
-                        })} >{element["datetime"]}</DataTable.Cell>
-                    </DataTable.Row>
-                </>
-                ))
-            });
-            this.setState({data:arr})
-
-        })
-    }
+				arr.push(
+					<>
+						<DataTable.Row>
+							<DataTable.Cell
+								onPress={() =>
+									navigation.navigate("chart", {
+										id: element["_id"],
+									})
+								}
+							>
+								{element["_id"]}
+							</DataTable.Cell>
+							<DataTable.Cell
+								onPress={() =>
+									navigation.navigate("chart", {
+										id: element["_id"],
+									})
+								}
+							>
+								{element["datetime"]}
+							</DataTable.Cell>
+						</DataTable.Row>
+					</>
+				);
+			});
+			this.setState({ data: arr });
+		});
+	}
 
 	render() {
-
-        
-
-        
 		return (
 			<View>
 				<DataTable>
@@ -58,10 +64,10 @@ export default class List extends Component {
 						<DataTable.Title numeric sortDirection="descending">
 							Data
 						</DataTable.Title>
-                        <DataTable.Title></DataTable.Title>
+						<DataTable.Title></DataTable.Title>
 					</DataTable.Header>
 
-                    {this.state.data}
+					{this.state.data}
 
 					<DataTable.Pagination
 						page={1}
