9d3bbfd63f0d4f6296ebf1ae23fea397c8f43e13
Marcelo da Silva Janke <marcelosjanke@icloud.com>
2021-05-26 17:13:51 -0300

    Update list.js

--- Components/list/list.js
+++ Components/list/list.js
@@ -64,7 +64,7 @@ export default class List extends Component {
 			<View>
 				<DataTable>
 					<DataTable.Header>
-						<DataTable.Title>IDExame</DataTable.Title>
+						<DataTable.Title>Paciente</DataTable.Title>
 						<DataTable.Title numeric sortDirection="descending">
 							Data
 						</DataTable.Title>
